fd36dbf02ac9ea4e788246e8df580b39a0ff07e2
yosmurakami <yosmurakami124@gmail.com>
2016-01-11 22:51:32 +0900

    coding navbar dropdown

--- FlatUIBlog/footer.php
+++ FlatUIBlog/footer.php
@@ -1,11 +1,8 @@
 <footer>
   <p>&copy; <?php bloginfo('name'); ?></p>
 </footer>
-</div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-
-  <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/bootstrap.min.js"></script>
-  <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+</div> <!-- /container -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 
   <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
   <script>
--- FlatUIBlog/js/main.js
+++ FlatUIBlog/js/main.js
@@ -1,8 +1,26 @@
-<script type="text/javascript">
 
 $(document).ready(function(){
-  //何かしらの処理
-  alert("esdf");
 });
 
-</script>
+$(function(){
+  /*
+    $('#main-nav li').hover(function(){
+        $("ul:not(:animated)", this).slideDown();
+    }, function(){
+        $("ul.sub-menu",this).slideUp();
+    });
+    */
+    $("ul#main-nav li").hover(function(){
+      $("> ul:not(:animated)" , this).animate({
+        height : "toggle",
+        opacity : "toggle"
+      }, 450 );
+      $(this).children("a").css("color","#1ABC9C");
+      $(this).children(".sub-menu li a").css("color","#1ABC9C");
+    },
+
+    function(){
+      $("> ul" , this).fadeOut("slow");
+      $("> a" , this).css("color","#5E5E5E");
+    });
+});
